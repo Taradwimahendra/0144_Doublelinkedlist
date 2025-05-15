8ee544358bc2807ca550102ec4b8d0ece7f30ca3
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-15 18:26:29 +0700

    menggunakan perulangan if untuk mengecek nim

--- file.cpp
+++ file.cpp
@@ -50,10 +50,17 @@ class DoubleLinkedList
             return;
         }
 
-         Node *current = START;
-          while (current->next != NULL && current->next->noMhs < nim)
+        
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
         {
            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
         }
 
 
